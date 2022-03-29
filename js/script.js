8664fea3e9e6d910342f54afb6b9f4077caf597d
Semral <semral.92@mail.ru>
2022-03-29 23:14:29 +0400

    // задание 1 - урок 12 - Вариант учителя

--- js/script.js
+++ js/script.js
@@ -1,21 +1,43 @@
 /*jslint node: true */
 'use strict';
 
+// **** задание 1 - урок 12 - Мой вариант ****
 
-let numberOfFilms = prompt ('Сколько фильмов вы уже посмотрели?',  "");
+// let numberOfFilms = prompt ('Сколько фильмов вы уже посмотрели?',  "");
 
-let lastFilm = prompt ('Один из последних просмотренных фильмов?',  "");
-let yourRate = prompt ('На сколько оцените его?',  "");
+// let lastFilm = prompt ('Один из последних просмотренных фильмов?',  "");
+// let yourRate = prompt ('На сколько оцените его?',  "");
 
-let personalMovieDB = {
+// let personalMovieDB = {
+//     count : numberOfFilms,
+//     movies : {
+//         lastFilm: yourRate,
+//     },
+//     actors : {},
+//     genres : [],
+//     privat : false,
+// };
+
+/////////////////////////////////////////////////////////////////
+
+// задание 1 - урок 12 - Вариант учителя
+
+const numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?',  '');
+
+const personalMovieDB = {
     count : numberOfFilms,
-    movies : {
-        lastFilm: yourRate,
-    },
+    movies : {},
     actors : {},
     genres : [],
-    privat : false,
+    privat : false
 };
 
+const a = prompt ('Один из последних просмотренных фильмов?', ''), 
+      b =  prompt ('На сколько оцените его?', ''),
+      c = prompt ('Один из последних просмотренных фильмов?', ''),
+      d =  prompt ('На сколько оцените его?', '');
 
-console.log (personalMovieDB);
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;  // не понял я почему по 2 раза спрашивает
+
+      console.log(personalMovieDB);
