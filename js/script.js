42a4c7b3ccef8f8f61a134c5a1950aee5371d3df
Semral <semral.92@mail.ru>
2022-03-31 22:57:28 +0400

    // задание 1 - урок 12 - Вариант учителя ver1.1

--- js/script.js
+++ js/script.js
@@ -22,22 +22,22 @@
 
 // задание 1 - урок 12 - Вариант учителя
 
-const numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?',  '');
+// const numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?',  '');
 
-const personalMovieDB = {
-    count : numberOfFilms,
-    movies : {},
-    actors : {},
-    genres : [],
-    privat : false
-};
+// const personalMovieDB = {
+//     count : numberOfFilms,
+//     movies : {},
+//     actors : {},
+//     genres : [],
+//     privat : false
+// };
 
-const a = prompt ('Один из последних просмотренных фильмов?', ''), 
-      b =  prompt ('На сколько оцените его?', ''),
-      c = prompt ('Один из последних просмотренных фильмов?', ''),
-      d =  prompt ('На сколько оцените его?', '');
+// const a = prompt ('Один из последних просмотренных фильмов?', ''), 
+//       b =  prompt ('На сколько оцените его?', ''),
+//       c = prompt ('Один из последних просмотренных фильмов?', ''),
+//       d =  prompt ('На сколько оцените его?', '');
 
-      personalMovieDB.movies[a] = b;
-      personalMovieDB.movies[c] = d;  // не понял я почему по 2 раза спрашивает
+//       personalMovieDB.movies[a] = b;
+//       personalMovieDB.movies[c] = d;  // не понял я почему по 2 раза спрашивает
 
-      console.log(personalMovieDB);
+//       console.log(personalMovieDB);
